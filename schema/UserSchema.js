025914e4ef8b3f6ed4ec2e2dfde2f1eb5d486b91
Vishal Magdum <71601212+VishalMagdum@users.noreply.github.com>
2023-04-18 10:33:02 +0530

    Update UserSchema.js

--- schema/UserSchema.js
+++ schema/UserSchema.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        validate: (value) => validator.isEmail(value)
+        validate:[ (value) => validator.isEmail(value),"plz Enter valid Email"]
     },
     mobile: {
         type: String,
